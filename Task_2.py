f74df18504e866ce4af1ffb28ca255db36203cb9
vishalvs5300-tech <vishalvs5300@gmail.com>
2025-11-07 18:06:01 +0530

    Initial commit

---
+++ Task_2.py
@@ -0,0 +1,7 @@
+  # Task 2: Create a Personalized Greeting
+
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+fullName = name1 + " " + name2 + "!"
+
+print("\nHello",fullName,"Welcome to the python program.")
